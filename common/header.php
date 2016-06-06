30ce985c4aa52ee1cc404dd838ebb6debaf16268
RyanMarten <ryanmarten2000@gmail.com>
2016-06-06 22:29:50 +0000

    Finishes styling header

--- common/header.php
+++ common/header.php
@@ -2,22 +2,24 @@
 <html>
 
 <head>
-    <!-- add all dependencies here -- Move to after body for better preformance later -->
-    <!-- Include jQuery -->
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <!-- add all dependencies here -- move scripts to after body for better preformance later -->
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="common/style.css">
     <script src="https://use.fontawesome.com/ba4f9520e8.js"></script>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <title>Deli Countr |  <?php echo $pageTitle ?> </title>
+    <title>Delicountr |  <?php echo $pageTitle ?> </title>
     <link rel="shortcut icon" type="image/x-icon" href="../favicon.ico" />
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
-    <div class="container-fluid">
-        <div class="header">
+    <noscript>This site won't work without Java Script</noscript>
+    <header>
+        <div class="header row">
+           <div class="title"> <p class="text-center">Delicountr</p></div>
         </div>
-    
+    </header>
+    <div class="container">
     <!-- Add basic page divs (remember to close the divs and body in the footer.php) -->
--- index.php
+++ index.php
@@ -1,24 +1,26 @@
 <?php include_once "common/header.php"; ?>
 
-<div id="main">
-
-   <noscript>This site won't update without Java Script</noscript>
-   <div class="row">
-      <div class="xs-col-8 xs-col-push-2">
-      <a href="#"><img class="img-reponsive" src="images/ticket.png" height="100" width="100"/></a>
+      <div id="main">
+      
+         
+         <!-- 
+         <div class="row">
+            <div class="xs-col-8 xs-col-push-2">
+            <a href="#"><img class="img-reponsive" src="images/ticket.png" height="100" width="100"/></a>
+            </div>
+         </div>
+         -->
+         
+         
+         <!-- IF IN QUEUE -->
+      
+                <!-- Content here -->
+      
+         <!-- IF NOT IN QUEUE -->
+      
+                <!-- Alternate content here -->
+      
       </div>
-   </div>
-   
-   
-   <!-- IF IN QUEUE -->
-
-          <!-- Content here -->
-
-   <!-- IF NOT IN QUEUE -->
-
-          <!-- Alternate content here -->
-
-</div>
 
 
 <?php include_once "common/footer.php"; ?>
